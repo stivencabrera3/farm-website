11d658cb9035a4ec2543dd7badfdc4a2de5acd6e
stivencabrera3 <stiven.cabrera3@gmail.com>
2019-06-23 02:51:05 -0400

    finish home-visit-us, start footer

--- main.js
+++ main.js
@@ -45,35 +45,42 @@
   }
 
 
+  // Opens and closed menu on top nav for mobile on menu button click
+
  const openMenu = function(){
    let sideNav = document.getElementById('side-nav')
    let main = document.querySelector('main')
    let topNav = document.getElementById('top-nav')
   
-   if (sideNav.style.display === 'none') {
+   if (sideNav.style.left === '-300px') {
 
-     sideNav.style.display = 'block'
+     sideNav.style.left = '0'
      main.style.marginLeft = '300px'
      topNav.style.marginLeft = '300px'
 
    } else {
-     sideNav.style.display = 'none'
-     main.style.marginLeft = '0'
-     topNav.style.marginLeft = '0'
+     sideNav.style.left = '-300px'
+     main.style.marginLeft = '0px'
+     topNav.style.marginLeft = '0px'
      
    }
  }
 
+//  Closes menu on click of close-btn
 
-//   const activeNav = function() {
-    
-//   if (document.getElementById('plus').style.display !== 'none') {
-//     document.getElementById('plus').style.display = 'none'
-//     document.getElementById('minus').style.display = 'inline'
-//   } else {
-//     document.getElementById('plus').style.display = 'inline'
-//     document.getElementById('minus').style.display = 'none'
-//   }
+const closeMenu = function(){
+  let sideNav = document.getElementById('side-nav')
+  let topNav = document.getElementById('top-nav')
+  let main = document.querySelector('main')
+  
 
-// }
+  if (sideNav.style.left === '0px') {
+    sideNav.style.left = '-300px'
+    main.style.marginLeft = '0px'
+    topNav.style.marginLeft = '0px'
+  }
+}
+
+
+
 
